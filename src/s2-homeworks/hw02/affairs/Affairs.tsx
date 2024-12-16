c61de1cab7338560a80ea5fc33e524507f4bc892
Aleksandr Ovsyannikov <116282471+AlexOneWRLD@users.noreply.github.com>
2024-12-16 17:30:59 +0300

    add HW2

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -1,77 +1,58 @@
-import React from 'react'
-import Affair from './affair/Affair'
-import {AffairType, FilterType} from '../HW2'
-import s from './Affairs.module.css'
+import React from 'react';
+import Affair from './affair/Affair';
+import { AffairType, FilterType } from '../HW2';
+import s from './Affairs.module.css';
 
+// Типизация пропсов
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
-    filter: FilterType
-}
+    data: AffairType[];
+    setFilter: (filter: FilterType) => void;
+    deleteAffairCallback: (id: number) => void;
+    filter: FilterType;
+};
 
-function Affairs(props: AffairsPropsType) {
-    const setAll = () => {
-        // need to fix
-    }
-    const setHigh = () => {
-        // need to fix
-    }
-    const setMiddle = () => {
-        // need to fix
-    }
-    const setLow = () => {
-        // need to fix
-    }
+const Affairs: React.FC<AffairsPropsType> = (props) => {
+    // Функции для установки фильтров
+    const setAll = () => props.setFilter('all');
+    const setHigh = () => props.setFilter('high');
+    const setMiddle = () => props.setFilter('middle');
+    const setLow = () => props.setFilter('low');
 
-    const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
-    const cnHigh = s.button + ' ' + s.high + (props.filter === 'high' ? ' ' + s.active : '')
-    const cnMiddle = s.button + ' ' + s.middle + (props.filter === 'middle' ? ' ' + s.active : '')
-    const cnLow = s.button + ' ' + s.low + (props.filter === 'low' ? ' ' + s.active : '')
+    // Определение классов для кнопок
+    const cnAll = `${s.button} ${s.all} ${props.filter === 'all' ? s.active : ''}`;
+    const cnHigh = `${s.button} ${s.high} ${props.filter === 'high' ? s.active : ''}`;
+    const cnMiddle = `${s.button} ${s.middle} ${props.filter === 'middle' ? s.active : ''}`;
+    const cnLow = `${s.button} ${s.low} ${props.filter === 'low' ? s.active : ''}`;
 
+    // Отображение дел
     const mappedAffairs = props.data.map((a: AffairType) => (
         <Affair
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+            key={a._id} // указывать уникальные ключи
             affair={a}
             deleteAffairCallback={props.deleteAffairCallback}
         />
-    ))
+    ));
 
     return (
         <div>
             <div className={s.buttonContainer}>
-                <button
-                    id={'hw2-button-all'}
-                    onClick={setAll}
-                    className={cnAll}
-                >
+                <button id="hw2-button-all" onClick={setAll} className={cnAll}>
                     All
                 </button>
-                <button
-                    id={'hw2-button-high'}
-                    onClick={setHigh}
-                    className={cnHigh}
-                >
+                <button id="hw2-button-high" onClick={setHigh} className={cnHigh}>
                     High
                 </button>
-                <button
-                    id={'hw2-button-middle'}
-                    onClick={setMiddle}
-                    className={cnMiddle}
-                >
+                <button id="hw2-button-middle" onClick={setMiddle} className={cnMiddle}>
                     Middle
                 </button>
-                <button
-                    id={'hw2-button-low'}
-                    onClick={setLow}
-                    className={cnLow}
-                >
+                <button id="hw2-button-low" onClick={setLow} className={cnLow}>
                     Low
                 </button>
             </div>
             <div className={s.affairs}>{mappedAffairs}</div>
         </div>
-    )
-}
+    );
+};
 
-export default Affairs
+export default Affairs;
+
